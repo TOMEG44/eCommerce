ba271d0c63aef2fe9364ede1e5bab830ecb76b77
TOMEG44 <tomaszoparakontakt@gmail.com>
2023-05-25 21:15:20 +0200

    product cart

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -13,8 +13,12 @@
             <h4>Description: {{$product['category']}}</h4>
             <h4>Description: {{$product['description']}}</h4>
         <br><br>
-        <button class="btn btn-primary">Add to Cart</button>
-        <button class="btn btn-success">Buy Now</button>
+        <form action="/add_to_cart" method="POST">
+            @csrf
+            <input type="hidden" name="product_id" value={{$product['id']}}>
+            <button class="btn btn-primary">Add to Cart</button>
+            <button class="btn btn-success">Buy Now</button>
+        </form>
         </div>
     </div>
 </div>
